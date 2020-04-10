bcf04dde0c0ce1a6d3a0c9d8d0dff1b26c9edca8
Sebastian Sterk <sebastian@wiuwiu.de>
2020-04-10 11:07:22 +0200

    Add cookie option

--- index.php
+++ index.php
@@ -4,5 +4,16 @@ echo "Hostname: ".gethostname();
 echo "\nServer IP: ".$_SERVER['SERVER_ADDR'];
 echo "\nClient IP: ".$_SERVER['REMOTE_ADDR'];
 echo "\nX-Forwarded-for: ".$_SERVER['HTTP_X_FORWARDED_FOR'];
+if($_GET['sticky']==1){
+  session_start();
+  print_r($_COOKIE);
+  if(!isset($_SESSION['visit'])){
+   echo "This is the first time you are visiting this server";
+   $_SESSION['visit'] = 0;
+  } else {
+   echo "Your number of visits: ".$_SESSION['visit'];
+   $_SESSION['visit']++;
+  }
+}
 ?>
 
