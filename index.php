ceeea0ec42c395fc8c2f0f6a18cd7bcfabeab919
Sebastian Sterk <sebastian@wiuwiu.de>
2020-04-10 11:16:41 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php
+if($_GET['sticky']==1){
+  session_start();
+}
+
 header('Content-Type: text/plain');
 echo "Hostname: ".gethostname();
 echo "\nServer IP: ".$_SERVER['SERVER_ADDR'];
 echo "\nClient IP: ".$_SERVER['REMOTE_ADDR'];
 echo "\nX-Forwarded-for: ".$_SERVER['HTTP_X_FORWARDED_FOR'];
 if($_GET['sticky']==1){
-  session_start();
   print_r($_COOKIE);
   if(!isset($_SESSION['visit'])){
    echo "This is the first time you are visiting this server";
